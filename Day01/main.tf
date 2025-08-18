0b2acd64c9a82fe478a646aa67250dba47e800d6
abdulraheem381 <shadowpulse.pentest@gmail.com>
2025-08-18 12:49:47 +0500

    Day01 code added

--- Day01/main.tf
+++ Day01/main.tf
@@ -1,1 +1,14 @@
-# code will be here soon!
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a simple EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-08c40ec9ead489470" # Amazon Linux 2 AMI (us-east-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-First-Instance"
+  }
+}
