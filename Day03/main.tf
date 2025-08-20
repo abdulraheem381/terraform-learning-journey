17edb89fded4b4d5c26a36073a9634f8130d66a7
abdulraheem381 <shadowpulse.pentest@gmail.com>
2025-08-20 12:09:48 +0500

    Day03 done

---
+++ Day03/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.9.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "my_vpc"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private-subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.my_vpc.id
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "public-subnet" {
+  cidr_block              = "10.0.2.0/24"
+  vpc_id                  = aws_vpc.my_vpc.id
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "my-rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-igw.id
+  }
+}
+
+# Route Table Association (Public Subnet)
+resource "aws_route_table_association" "public-sub" {
+  route_table_id = aws_route_table.my-rt.id
+  subnet_id      = aws_subnet.public-subnet.id
+}
