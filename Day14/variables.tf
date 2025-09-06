f4542a0f6ab73876c7c0811857fbdf4bea5e9c05
abdulraheem381 <shadowpulse.pentest@gmail.com>
2025-09-06 12:17:36 +0500

    variables added

---
+++ Day14/variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "day14demo"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
