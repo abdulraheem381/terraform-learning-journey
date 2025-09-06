1a7d021fb734424ce1d04212e276173b80e43f4b
abdulraheem381 <shadowpulse.pentest@gmail.com>
2025-09-06 12:16:45 +0500

    Main file added

---
+++ Day14/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.9.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+# Example of expressions
+resource "aws_s3_bucket" "example" {
+  bucket = "${var.project_name}-${terraform.workspace}"
+
+  tags = {
+    Name        = var.project_name
+    Environment = terraform.workspace
+  }
+}
+
+# Example of functions
+locals {
+  upper_project = upper(var.project_name)
+  bucket_name   = format("%s-%s", var.project_name, random_integer.rand.result)
+}
+
+resource "random_integer" "rand" {
+  min = 100
+  max = 999
+}
+
+resource "aws_s3_bucket" "with_function" {
+  bucket = local.bucket_name
+}
